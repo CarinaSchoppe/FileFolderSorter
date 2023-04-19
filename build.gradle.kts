df01ea6abecf99e0c07ba0cfb6f7c640868d540b
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2023-04-19 20:19:11 +0200

    commit auf gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "+"
-    java
     //javafx plugin
     id("application")
     id("com.github.johnrengelman.shadow") version "+"
